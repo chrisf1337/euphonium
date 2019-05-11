680c48bdfb4250121025b0d972799412f610fbad
Christopher Fu <chris@christopherfu.me>
2019-05-11 15:20:42 -0700

    Undo generics

--- src/ast.rs
+++ src/ast.rs
@@ -1,37 +1,14 @@
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Decl<Expr: StrippableExpr> {
-    pub ty: DeclType<Expr>,
+pub struct Decl {
+    pub ty: DeclType,
     pub span: Span,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct StrippedDecl {
-    pub ty: DeclType<StrippedExpr>,
-}
-
-impl<Expr: StrippableExpr> Decl<Expr> {
-    pub fn strip(self) -> StrippedDecl {
-        StrippedDecl {
-            ty: self.ty.strip(),
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum DeclType<Expr: StrippableExpr> {
+pub enum DeclType {
     Type(TypeDecl),
-    Fn(FnDecl<Expr>),
+    Fn(FnDecl),
     Error,
-}
-
-impl<Expr: StrippableExpr> DeclType<Expr> {
-    fn strip(self) -> DeclType<StrippedExpr> {
-        match self {
-            DeclType::Type(decl) => DeclType::Type(decl),
-            DeclType::Fn(decl) => DeclType::Fn(decl.strip()),
-            DeclType::Error => DeclType::Error,
-        }
-    }
 }
 
 /// type A = ...
@@ -62,38 +39,17 @@ pub enum Pattern {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Let<Expr: StrippableExpr> {
+pub struct Let {
     pub pattern: Pattern,
     pub mutable: bool,
     pub ty: Option<Type>,
     pub expr: Expr,
 }
 
-impl<Expr: StrippableExpr> Let<Expr> {
-    fn strip(self) -> Let<StrippedExpr> {
-        Let {
-            pattern: self.pattern,
-            mutable: self.mutable,
-            ty: self.ty,
-            expr: self.expr.strip(),
-        }
-    }
-}
-
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub struct Span {
     pub l: usize,
     pub r: usize,
-}
-
-pub trait StrippableExpr {
-    fn strip(self) -> StrippedExpr;
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Expr {
-    pub ty: ExprType<Self>,
-    pub span: Span,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -103,44 +59,18 @@ pub struct Spanned<T> {
 }
 
 impl<T> Spanned<T> {
-    pub fn unwrap(self) -> T {
-        self.t
-    }
-}
-
-impl StrippableExpr for Expr {
-    fn strip(self) -> StrippedExpr {
-        StrippedExpr {
-            ty: self.ty.strip(),
+    pub fn new(t: T, span: (usize, usize)) -> Spanned<T> {
+        Spanned {
+            t,
+            span: Span { l: span.0, r: span.1 }
         }
     }
 }
 
-impl Expr {
-    pub fn new(ty: ExprType<Self>, span: (usize, usize)) -> Self {
-        Self {
-            ty,
-            span: Span {
-                l: span.0,
-                r: span.1,
-            },
-        }
-    }
-}
+pub type Expr = Spanned<ExprType>;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct StrippedExpr {
-    pub ty: ExprType<Self>,
-}
-
-impl StrippableExpr for StrippedExpr {
-    fn strip(self) -> StrippedExpr {
-        self
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum ExprType<Expr: StrippableExpr> {
+pub enum ExprType {
     /// true if returns last value, false if unit-typed
     Seq(Vec<Expr>, bool),
     String(String),
@@ -153,57 +83,17 @@ pub enum ExprType<Expr: StrippableExpr> {
     Bool(Box<Expr>, BoolOp, Box<Expr>),
     Continue,
     Break,
-    LVal(Box<LVal<Expr>>),
-    Let(Box<Let<Expr>>),
-    FnCall(Box<FnCall<Expr>>),
-    Record(Box<Record<Expr>>),
-    Assign(Box<Assign<Expr>>),
-    Array(Box<Array<Expr>>),
-    If(Box<If<Expr>>),
+    LVal(Box<LVal>),
+    Let(Box<Let>),
+    FnCall(Box<FnCall>),
+    Record(Box<Record>),
+    Assign(Box<Assign>),
+    Array(Box<Array>),
+    If(Box<If>),
     Range(Box<Expr>, Box<Expr>),
-    For(Box<For<Expr>>),
-    While(Box<While<Expr>>),
+    For(Box<For>),
+    While(Box<While>),
     Compare(Box<Expr>, CompareOp, Box<Expr>),
-}
-
-impl<Expr: StrippableExpr> ExprType<Expr> {
-    fn strip(self) -> ExprType<StrippedExpr> {
-        match self {
-            ExprType::Seq(exprs, returns) => ExprType::Seq(
-                exprs.into_iter().map(StrippableExpr::strip).collect(),
-                returns,
-            ),
-            ExprType::String(s) => ExprType::String(s),
-            ExprType::Number(n) => ExprType::Number(n),
-            ExprType::Neg(expr) => ExprType::Neg(Box::new(expr.strip())),
-            ExprType::Arith(l, op, r) => {
-                ExprType::Arith(Box::new(l.strip()), op, Box::new(r.strip()))
-            }
-            ExprType::Unit => ExprType::Unit,
-            ExprType::BoolLiteral(b) => ExprType::BoolLiteral(b),
-            ExprType::Not(b) => ExprType::Not(Box::new(b.strip())),
-            ExprType::Bool(l, op, r) => {
-                ExprType::Bool(Box::new(l.strip()), op, Box::new(r.strip()))
-            }
-            ExprType::Continue => ExprType::Continue,
-            ExprType::Break => ExprType::Break,
-            ExprType::LVal(expr) => ExprType::LVal(Box::new(expr.strip())),
-            ExprType::Let(expr) => ExprType::Let(Box::new(expr.strip())),
-            ExprType::FnCall(expr) => ExprType::FnCall(Box::new(expr.strip())),
-            ExprType::Record(expr) => ExprType::Record(Box::new(expr.strip())),
-            ExprType::Assign(expr) => ExprType::Assign(Box::new(expr.strip())),
-            ExprType::Array(expr) => ExprType::Array(Box::new(expr.strip())),
-            ExprType::If(expr) => ExprType::If(Box::new(expr.strip())),
-            ExprType::Range(from, to) => {
-                ExprType::Range(Box::new(from.strip()), Box::new(to.strip()))
-            }
-            ExprType::For(expr) => ExprType::For(Box::new(expr.strip())),
-            ExprType::While(expr) => ExprType::While(Box::new(expr.strip())),
-            ExprType::Compare(l, op, r) => {
-                ExprType::Compare(Box::new(l.strip()), op, Box::new(r.strip()))
-            }
-        }
-    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -227,165 +117,66 @@ pub enum CompareOp {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct FnDecl<Expr: StrippableExpr> {
+pub struct FnDecl {
     pub id: String,
     pub type_fields: Vec<TypeField>,
     pub return_type: Option<Type>,
     pub body: Expr,
 }
 
-impl<Expr: StrippableExpr> FnDecl<Expr> {
-    fn strip(self) -> FnDecl<StrippedExpr> {
-        FnDecl {
-            id: self.id,
-            type_fields: self.type_fields,
-            return_type: self.return_type,
-            body: self.body.strip(),
-        }
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum LVal<Expr: StrippableExpr> {
+pub enum LVal {
     Simple(String),
-    Field(Box<LVal<Expr>>, String),
-    Subscript(Box<LVal<Expr>>, Expr),
-}
-
-impl<Expr: StrippableExpr> LVal<Expr> {
-    fn strip(self) -> LVal<StrippedExpr> {
-        match self {
-            LVal::Simple(s) => LVal::Simple(s),
-            LVal::Field(lval, field) => LVal::Field(Box::new(lval.strip()), field),
-            LVal::Subscript(lval, sub) => LVal::Subscript(Box::new(lval.strip()), sub.strip()),
-        }
-    }
+    Field(Box<LVal>, String),
+    Subscript(Box<LVal>, Expr),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct FnCall<Expr: StrippableExpr> {
+pub struct FnCall {
     pub id: String,
     pub args: Vec<Expr>,
 }
 
-impl<Expr: StrippableExpr> FnCall<Expr> {
-    fn strip(self) -> FnCall<StrippedExpr> {
-        FnCall {
-            id: self.id,
-            args: self.args.into_iter().map(StrippableExpr::strip).collect(),
-        }
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Record<Expr: StrippableExpr> {
+pub struct Record {
     pub id: String,
-    pub field_assigns: Vec<FieldAssign<Expr>>,
-}
-
-impl<Expr: StrippableExpr> Record<Expr> {
-    fn strip(self) -> Record<StrippedExpr> {
-        Record {
-            id: self.id,
-            field_assigns: self
-                .field_assigns
-                .into_iter()
-                .map(FieldAssign::strip)
-                .collect(),
-        }
-    }
+    pub field_assigns: Vec<FieldAssign>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct FieldAssign<Expr: StrippableExpr> {
+pub struct FieldAssign {
     pub id: String,
     pub expr: Expr,
 }
 
-impl<Expr: StrippableExpr> FieldAssign<Expr> {
-    fn strip(self) -> FieldAssign<StrippedExpr> {
-        FieldAssign {
-            id: self.id,
-            expr: self.expr.strip(),
-        }
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Assign<Expr: StrippableExpr> {
-    pub lval: LVal<Expr>,
+pub struct Assign {
+    pub lval: LVal,
     pub expr: Expr,
 }
 
-impl<Expr: StrippableExpr> Assign<Expr> {
-    fn strip(self) -> Assign<StrippedExpr> {
-        Assign {
-            lval: self.lval.strip(),
-            expr: self.expr.strip(),
-        }
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Array<Expr: StrippableExpr> {
+pub struct Array {
     pub initial_value: Expr,
     pub len: Expr,
 }
 
-impl<Expr: StrippableExpr> Array<Expr> {
-    fn strip(self) -> Array<StrippedExpr> {
-        Array {
-            initial_value: self.initial_value.strip(),
-            len: self.len.strip(),
-        }
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct If<Expr: StrippableExpr> {
+pub struct If {
     pub cond: Expr,
     pub then_expr: Expr,
     pub else_expr: Option<Expr>,
 }
 
-impl<Expr: StrippableExpr> If<Expr> {
-    fn strip(self) -> If<StrippedExpr> {
-        If {
-            cond: self.cond.strip(),
-            then_expr: self.then_expr.strip(),
-            else_expr: self.else_expr.map(StrippableExpr::strip),
-        }
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct For<Expr> {
+pub struct For {
     pub index: String,
     pub range: Expr,
     pub body: Expr,
 }
 
-impl<Expr: StrippableExpr> For<Expr> {
-    fn strip(self) -> For<StrippedExpr> {
-        For {
-            index: self.index,
-            range: self.range.strip(),
-            body: self.body.strip(),
-        }
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct While<Expr: StrippableExpr> {
+pub struct While {
     pub cond: Expr,
     pub body: Expr,
-}
-
-impl<Expr: StrippableExpr> While<Expr> {
-    fn strip(self) -> While<StrippedExpr> {
-        While {
-            cond: self.cond.strip(),
-            body: self.body.strip(),
-        }
-    }
 }
